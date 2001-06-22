95c3538c8f5c6fcdf1c70bf583e07a60c2f1cb52
Kenneth Russel <kbrussel@alum.mit.edu>
2001-06-22 04:27:30 +0000

    *** empty log message ***

---
+++ C2J/glf-enum-auto.orig.h
@@ -0,0 +1,29 @@
+typedef enum {
+ GLF_ERROR           =-1,
+ GLF_OK              = 0,
+ GLF_YES             = 1,
+ GLF_NO              = 2,
+
+ GLF_CONSOLE_MESSAGES =10,
+ GLF_TEXTURING        =11,
+ GLF_CONTOURING       =12,
+
+ GLF_LEFT_UP          =20,
+ GLF_LEFT_CENTER      =21,
+ GLF_LEFT_DOWN        =22,
+ GLF_CENTER_UP        =23,
+ GLF_CENTER_CENTER    =24,
+ GLF_CENTER_DOWN      =25,
+ GLF_RIGHT_UP         =26,
+ GLF_RIGHT_CENTER     =27,
+ GLF_RIGHT_DOWN       =28,
+ GLF_CENTER           =24,
+
+ GLF_LEFT	=1,
+ GLF_RIGHT	=2,
+ GLF_UP		=3,
+ GLF_DOWN	=4,
+
+/* Console constants */
+ GLF_CONSOLE_CURSOR   =30
+};
